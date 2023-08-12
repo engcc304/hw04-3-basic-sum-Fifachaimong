f37b6afef852679c7f7271521f09777ae7ff8bc3
your name <fifasisa36@gmail.com>
2023-08-13 01:30:08 +0700

    hw 4-3

--- Homework/HW04/hw04-3-basic-operator.cpp
+++ Homework/HW04/hw04-3-basic-operator.cpp
@@ -13,3 +13,26 @@
         Answer = 6
 */
 #include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Input:\n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    int answer = a + b + c;
+
+    printf("Output:\n");
+    printf("Answer = %d\n", answer);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
